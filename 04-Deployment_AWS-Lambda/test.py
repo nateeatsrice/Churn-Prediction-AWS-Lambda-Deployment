3008d41ec4bd2b4450df1a3fc569cfd655bbea80
Nathaniel Rice-Canetto <nathaniel.c.rice@gmail.com>
2025-08-29 15:06:05 +0000

    added test.py and added boto3 to uv environment

---
+++ 04-Deployment_AWS-Lambda/test.py
@@ -0,0 +1,53 @@
+import requests
+
+url = 'http://localhost:8080/2015-03-31/functions/function/invocations'
+
+customer = {
+    'gender': 'male',
+    'seniorcitizen': 1,
+    'partner': 'yes',
+    'dependents': 'yes',
+    'phoneservice': 'no',
+    'multiplelines': 'no_phone_service',
+    'internetservice': 'dsl',
+    'onlinesecurity': 'no',
+    'onlinebackup': 'yes',
+    'deviceprotection': 'no',
+    'techsupport': 'no',
+    'streamingtv': 'no',
+    'streamingmovies': 'no',
+    'contract': 'month-to-month',
+    'paperlessbilling': 'yes',
+    'paymentmethod': 'electronic_check',
+    'tenure': 1,
+    'monthlycharges': 30.85,
+    'totalcharges': 29.85
+}
+
+customer_2 = {
+    'gender': 'female',
+    'seniorcitizen': 0,
+    'partner': 'yes',
+    'dependents': 'yes',
+    'phoneservice': 'no',
+    'multiplelines': 'no_phone_service',
+    'internetservice': 'dsl',
+    'onlinesecurity': 'no',
+    'onlinebackup': 'yes',
+    'deviceprotection': 'no',
+    'techsupport': 'no',
+    'streamingtv': 'no',
+    'streamingmovies': 'no',
+    'contract': 'month-to-month',
+    'paperlessbilling': 'yes',
+    'paymentmethod': 'electronic_check',
+    'tenure': 1,
+    'monthlycharges': 29.85,
+    'totalcharges': 29.85
+}
+
+response = requests.post(url, json=customer)
+
+predictions = response.json()
+
+print(predictions)
